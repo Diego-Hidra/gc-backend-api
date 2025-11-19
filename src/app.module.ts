a55725bb86efe25aaf176666cb8c274568664e1a
Alex Sagredo Burgos <alexsagredo1204@gmail.com>
2025-11-18 23:08:20 -0300

    Adicion de dependencias instaladas con anterioridad

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ResidentModule } from './modules/resident.module';
+import { AuthModule } from './modules/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: parseInt(process.env.DB_PORT || '5342', 10),
+      username: process.env.USERNAME,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
+
+      entities: [
+        __dirname + '/**/*.entity{.ts,.js}', 
+      ],
+
+      synchronize: true
+    }),
+    ResidentModule, AuthModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
